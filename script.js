a87ed60791eb7500a14bbb8f0bef3dcdf55e59d8
bannymomo <bannymomo33@gmail.com>
2020-01-02 19:28:32 +1000

    conbine finsh and start botton

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 //get elements
-const startBtn = document.getElementById("startBtn");
-const finishBtn = document.getElementById("finishBtn");
+const botton = document.getElementById("botton");
 const gameContainer = document.getElementById("gameContainer");
 const currentLevel = document.getElementById("level");
 const second = document.getElementById("timer");
 const score = document.getElementById("score");
 
 //card pattern
-const cardPool = ["Iron-man", "Thor", "Natasha", "Hawkeye", "Spider-man", "Captain", "wizart"];
+const cardPool = [
+  "Iron-man",
+  "Thor",
+  "Natasha",
+  "Hawkeye",
+  "Spider-man",
+  "Captain",
+  "wizart"
+];
 
 //global variable
 let flipped;
@@ -22,196 +29,195 @@ let id;
 let cardNumber;
 let levelContainer;
 
-function gameStartInitial(){
-    gameContainer.style.display = "block";
-    levelNumber = 1;
-    currentLevel.innerHTML = levelNumber;
-    startBtn.style.display = "none";
-    finishBtn.style.display = "block";
-    waitingForFlip = false;
-    flipped = false;
-    firstCard = null;
-    secondCard = null;
-    second.innerHTML = 60; 
-    finalScore = 0;
-    score.innerHTML = finalScore;
-    counting = 0;
-    if(gameContainer.contains(levelContainer)){
-        gameContainer.removeChild(levelContainer)
-    }
+function gameStartInitial() {
+  gameContainer.style.display = "block";
+  levelNumber = 1;
+  currentLevel.innerHTML = levelNumber;
+  waitingForFlip = false;
+  flipped = false;
+  firstCard = null;
+  secondCard = null;
+  second.innerHTML = 60;
+  finalScore = 0;
+  score.innerHTML = finalScore;
+  counting = 0;
+  if (gameContainer.contains(levelContainer)) {
+    gameContainer.removeChild(levelContainer);
+  }
 }
 
-startBtn.onclick = function(){
+botton.onclick = function() {
+  if (botton.innerHTML === "Start") {
+    botton.innerHTML = "Finish";
     gameStartInitial();
     initUI(levelNumber);
     game();
+  } else {
+    botton.innerHTML = "Start";
+    gameOverInitial();
+    alert("Game over! Your score is " + finalScore + "!");
+  }
 };
 
-function pickCardsPattern(pairNumber){
-    const cards = [];
-    const cardPoolLength = cardPool.length;
+function pickCardsPattern(pairNumber) {
+  const cards = [];
+  const cardPoolLength = cardPool.length;
 
-    for (let i = 0; i < pairNumber; i++) {
-        let index;
-        if(i > cardPoolLength - 1 ){
-            index = i % cardPoolLength;
-        } else {
-            index = i;
-        }
-        const card = cardPool[index];
-        cards.push(card);
-        cards.push(card);
-    } 
-    return cards;
-};
-
-function createCard(shuffledcardsPattern){
-    levelContainer = document.createElement("div");
-    levelContainer.classList.add("levelContainer");
-    levelContainer.style['grid-template-columns'] = `repeat(${levelNumber * 2}, 1fr)`;
-    gameContainer.appendChild(levelContainer);
-    for(let i = 0; i < shuffledcardsPattern.length; i++){
-        const singleCard = document.createElement("div");
-        singleCard.classList.add("single-card-container");
-        singleCard.dataset.framework = shuffledcardsPattern[i];
-        levelContainer.appendChild(singleCard);
-        const frontFace = document.createElement("img");
-        singleCard.appendChild(frontFace);
-        frontFace.classList.add("front-face");
-        frontFace.src = "./img/"+shuffledcardsPattern[i]+".jpg";
-        const backFace = document.createElement("img");
-        singleCard.appendChild(backFace);
-        backFace.classList.add("back-face");
-        backFace.src = "./img/Avenger-Logo.jpg";
+  for (let i = 0; i < pairNumber; i++) {
+    let index;
+    if (i > cardPoolLength - 1) {
+      index = i % cardPoolLength;
+    } else {
+      index = i;
     }
+    const card = cardPool[index];
+    cards.push(card);
+    cards.push(card);
+  }
+  return cards;
 }
 
-function shuffleCards(cards){
-    return cards.sort(() => Math.random() - 0.5);
-};
-
-function initUI(levelNumber){
-    pairNumber = (levelNumber * 2) ** 2 / 2;
-    const cards = pickCardsPattern(pairNumber);
-    const shuffledCards = shuffleCards(cards);
-    createCard(shuffledCards);
+function createCard(shuffledcardsPattern) {
+  levelContainer = document.createElement("div");
+  levelContainer.classList.add("levelContainer");
+  levelContainer.style["grid-template-columns"] = `repeat(${levelNumber *
+    2}, 1fr)`;
+  gameContainer.appendChild(levelContainer);
+  for (let i = 0; i < shuffledcardsPattern.length; i++) {
+    const singleCard = document.createElement("div");
+    singleCard.classList.add("single-card-container");
+    singleCard.dataset.framework = shuffledcardsPattern[i];
+    levelContainer.appendChild(singleCard);
+    const frontFace = document.createElement("img");
+    singleCard.appendChild(frontFace);
+    frontFace.classList.add("front-face");
+    frontFace.src = "./img/" + shuffledcardsPattern[i] + ".jpg";
+    const backFace = document.createElement("img");
+    singleCard.appendChild(backFace);
+    backFace.classList.add("back-face");
+    backFace.src = "./img/Avenger-Logo.jpg";
+  }
 }
 
-function game(){
-    const cards = document.getElementsByClassName("single-card-container");
-    cardNumber = cards.length;
-    counting = 0;
+function shuffleCards(cards) {
+  return cards.sort(() => Math.random() - 0.5);
+}
 
-//cards add listener;
-    addListener(cards, flipCard);
+function initUI(levelNumber) {
+  pairNumber = (levelNumber * 2) ** 2 / 2;
+  const cards = pickCardsPattern(pairNumber);
+  const shuffledCards = shuffleCards(cards);
+  createCard(shuffledCards);
+}
 
-//timer 60s countdown
-    id = setInterval(countdown, 1000);
+function game() {
+  const cards = document.getElementsByClassName("single-card-container");
+  cardNumber = cards.length;
+  counting = 0;
+
+  //cards add listener;
+  addListener(cards, flipCard);
+
+  //timer 60s countdown
+  id = setInterval(countdown, 1000);
 }
 
 function countdown() {
-    second.innerHTML = second.innerHTML - 1;
-    alertLose();
-} 
-
-function addListener(cards) {
-    for(let i = 0; i < cards.length; i++){
-        cards[i].addEventListener("click", flipCard);
-    }
+  second.innerHTML = second.innerHTML - 1;
+  alertLose();
 }
 
-function flipCard(event){
-//if two cards not match, player can't flip the third card before these two cards flip back
-    if(waitingForFlip){
-        return;
+function addListener(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", flipCard);
+  }
+}
+
+function flipCard(event) {
+  //if two cards not match, player can't flip the third card before these two cards flip back
+  if (waitingForFlip) {
+    return;
+  }
+
+  event.currentTarget.classList.add("flip");
+
+  //choose first card and second card
+  if (!flipped) {
+    flipped = true;
+    firstCard = event.currentTarget;
+  } else {
+    flipped = false;
+    secondCard = event.currentTarget;
+
+    // select same card
+    if (firstCard === secondCard) {
+      firstCard.classList.remove("flip");
+      return;
     }
-
-    event.currentTarget.classList.add("flip");   
-
-//choose first card and second card
-    if (!flipped) {
-        flipped = true;
-        firstCard = event.currentTarget;
-    } else {
-        flipped = false;
-        secondCard = event.currentTarget;
-
-// select same card 
-    if(firstCard === secondCard){
-        firstCard.classList.remove("flip");
-        return;
-    }
-// two cards matching
+    // two cards matching
     if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        firstCard.removeEventListener("click",flipCard);
-        secondCard.removeEventListener("click",flipCard);
-        counting++;
-        secondInnerHtml = second.innerHTML;
-        const scoreincrease = levelNumber * levelNumber * secondInnerHtml;
-        finalScore += scoreincrease;
-        score.innerHTML = finalScore;
-        judge();
+      firstCard.removeEventListener("click", flipCard);
+      secondCard.removeEventListener("click", flipCard);
+      counting++;
+      secondInnerHtml = second.innerHTML;
+      const scoreincrease = levelNumber * levelNumber * secondInnerHtml;
+      finalScore += scoreincrease;
+      score.innerHTML = finalScore;
+      judge();
     } else {
-// two cards different
-        waitingForFlip = true;
-        setTimeout(() =>{
-            firstCard.classList.remove("flip");
-            secondCard.classList.remove("flip"); 
-            waitingForFlip = false; 
-            },1000);
-        return;
-        }
-    }   
+      // two cards different
+      waitingForFlip = true;
+      setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+        waitingForFlip = false;
+      }, 1000);
+      return;
+    }
+  }
 }
 
 function judge() {
-    // final judge
-    // win
-    if(secondInnerHtml > 0 && counting === cardNumber / 2) {
-        setTimeout(() => {        
-            alertWin();
-        }, 500);
-    }  
+  // final judge
+  // win
+  if (secondInnerHtml > 0 && counting === cardNumber / 2) {
+    setTimeout(() => {
+      alertWin();
+    }, 500);
+  }
 }
 
-function alertWin(){
-    //final level 3 win
-    if(levelNumber ===3) {
-        alert("You win! The final score is "+ finalScore +" !");
-        gameOverInitial();
-    } else {
+function alertWin() {
+  //final level 3 win
+  if (levelNumber === 3) {
+    alert("You win! The final score is " + finalScore + " !");
+    gameOverInitial();
+  } else {
     //level 1 or level 2 win
-        alert("You win!");
-        clearInterval(id);
-        gameContainer.removeChild(levelContainer);
-        nextLevel();
-    }
-}
-
-function nextLevel(){
-    levelNumber += 1;
-    currentLevel.innerHTML = levelNumber;
-    second.innerHTML = 60; 
-    initUI(levelNumber);
-    game();
-}
-
-function alertLose(){
-    if (second.innerHTML <= 0){
-    alert("Ops! Game over...Your score is " + finalScore + "...");
-    gameOverInitial();
-    } 
-}
-
-function gameOverInitial(){
+    alert("You win!");
     clearInterval(id);
-    waitingForFlip = true;
-    startBtn.style.display = "block";
-    finishBtn.style.display = "none";
-    flipped = false;
+    gameContainer.removeChild(levelContainer);
+    nextLevel();
+  }
 }
 
-finishBtn.onclick = function(){
+function nextLevel() {
+  levelNumber += 1;
+  currentLevel.innerHTML = levelNumber;
+  second.innerHTML = 60;
+  initUI(levelNumber);
+  game();
+}
+
+function alertLose() {
+  if (second.innerHTML <= 0) {
+    alert("Ops! Game over... Your score is " + finalScore + "...");
     gameOverInitial();
-    alert("Game over! Your score is " + finalScore + "!");
+  }
+}
+
+function gameOverInitial() {
+  clearInterval(id);
+  waitingForFlip = true;
+  flipped = false;
 }
